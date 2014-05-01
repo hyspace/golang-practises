22169de7360e2adaad0b7ccdbec1390fd0974ac4
shan zhou <hyspace@gmail.com>
2014-05-01 22:13:19 +0800

    Finished coursersa problem

--- count-inversions/countInversions/countInversions.go
+++ count-inversions/countInversions/countInversions.go
@@ -7,17 +7,25 @@ func mergeAndCount(dest []int, left []int, right []int) int {
 
   tmp := make([]int, length)
   result := 0
-  for i:=0, j:=0, k:=0; i < lenLeft && j < lenRight; ++k{
+  i := 0
+  j := 0
+  k := 0
+  for ; i < lenLeft && j < lenRight; k++{
     if left[i] <= right[j]{
       tmp[k] = left[i]
-      ++i
+      i++
     } else {
       tmp[k] = right[j]
       result += lenLeft - i
-      ++j
+      j++
     }
   }
-  if lenLeft
+  for ; i < lenLeft; i, k = i + 1, k + 1{
+    tmp[k] = left[i]
+  }
+  for ; j < lenRight; j, k = j + 1, k + 1{
+    tmp[k] = right[j]
+  }
 
   copy(dest, tmp)
   return result
@@ -25,6 +33,11 @@ func mergeAndCount(dest []int, left []int, right []int) int {
 
 func Count(slice []int) int {
   length := len(slice)
+
+  if length < 2 {
+    return 0
+  }
+
   mid := length / 2
 
   left := slice[0:mid]
--- count-inversions/main.go
+++ count-inversions/main.go
@@ -1,1 +1,59 @@
-main.go
+package main
+
+import(
+  "./countInversions"
+  "fmt"
+  "os"
+  "bufio"
+  "strconv"
+)
+
+func bruteForce(slice []int) int {
+  length := len(slice)
+  count := 0
+  for i := 0; i < length; i++ {
+    num := slice[i]
+    for j := i + 1; j < length; j++ {
+      if slice[j] < num {
+        count++
+      }
+    }
+  }
+  return count
+}
+
+func test(){
+  array1 := []int{6,10,13,5,8,2,11,15,4,25,21,5,73,1,46,3,6,31,5,7}
+  array2 := make([]int, len(array1))
+  copy(array2, array1)
+  count := countInversions.Count(array1)
+  fmt.Println(count)
+  fmt.Println(bruteForce(array2))
+}
+
+func main(){
+  file, err := os.Open("./IntegerArray.txt")
+  if err != nil {
+    panic(err)
+  }
+  defer file.Close()
+
+  var slice []int
+
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    integer, err := strconv.Atoi(scanner.Text())
+    if err != nil {
+      panic("convert failed")
+    }
+    slice = append(slice, integer)
+  }
+  if scanner.Err() != nil {
+    panic("error happened when reading file")
+  }
+  fmt.Printf("length of input: %d\n", len(slice))
+  count := countInversions.Count(slice)
+  // count := bruteForce(slice)
+  fmt.Println(count)
+
+}
